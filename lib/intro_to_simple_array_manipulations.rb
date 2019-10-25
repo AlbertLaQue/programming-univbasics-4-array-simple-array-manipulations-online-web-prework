bb3ecc9211ea48c292af1a4844479e8d3d58de9d
Albert LaQue <albertlaque@gmail.com>
2019-10-25 06:33:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,7 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
-  using_unshift = ["array","string", "Staten Island"]
- using_unshift.unshift("Staten Island")
+  famous_cats = ["lil' bub", "grumpy cat", "Maru","Staten Island"]
+ famous_cats.unshift("Staten Island")
+ 
 end
