1f87c822ad2b3675a61fa86f08242cdf77d52207
Albert LaQue <albertlaque@gmail.com>
2019-10-25 06:43:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,7 +7,7 @@ def using_unshift(array, string)
  famous_cats.unshift("Staten Island")
 end
 
-def using_pop
+def using_pop(array, element)
   famous_cats = ["lil' bub", "grumpy cat", "Antarctica"]
 maru_cat = famous_cats.pop
  end
